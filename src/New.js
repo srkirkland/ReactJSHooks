f1ea761294c83b93bc055872f63b2c909a145556
Unknown <srkirkland@ucdavis.edu>
2019-04-04 11:05:20 -0700

    start setting state

--- src/New.js
+++ src/New.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function(props) {
+  const [email, setEmail] = useState(props.first + '@gmail.com');
+
+  const handleEmailChange = e => setEmail(e.target.value);
+
   return (
-    <div className="row">
-      <h2>
-        {props.first} {props.last}
-      </h2>
+    <div>
+      <div className="row">
+        <input value={email} onChange={handleEmailChange} />
+      </div>
+      <div className="row">
+        <h2>
+          {props.first} {props.last} ({email})
+        </h2>
+      </div>
     </div>
   );
 }
--- src/Old.js
+++ src/Old.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
+// 2. Class with state
 export default class Old extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: props.first + '@gmail.com'
+    };
+  }
+
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+
   render() {
     return (
-      <div className="row">
-        <h2>
-          {this.props.first} {this.props.last}
-        </h2>
+      <div>
+        <div className="row">
+          <input value={this.state.email} onChange={this.handleEmailChange} />
+        </div>
+        <div className="row">
+          <h2>
+            {this.props.first} {this.props.last} ({this.state.email})
+          </h2>
+        </div>
       </div>
     );
   }
