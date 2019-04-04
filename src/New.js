73b1d8fb883d4198dc9284adcec26110f1461b61
Unknown <srkirkland@ucdavis.edu>
2019-04-04 11:45:14 -0700

    refactor state logic

--- src/New.js
+++ src/New.js
@@ -1,47 +1,61 @@
 import React, { useState } from 'react';
 
 export default function(props) {
-  const [email, setEmail] = useState(props.first + '@gmail.com');
-  const [eid, setEid] = useState('');
-  const [eidValid, setEidValid] = useState(true);
-
-  const handleEmailChange = e => setEmail(e.target.value);
-  const handleEIDChange = e => {
-    const eid = e.target.value;
-
-    if (eid.includes('9')) {
-      setEidValid(false);
-    } else {
-      setEidValid(true)
-      setEid(eid);
-    }
-  }
+  const email = useInput(props.first + '@gmail.com');
+  const eid = useEid();
 
   return (
     <div>
       <div className="row">
-        <input value={email} onChange={handleEmailChange} />
+        <input value={email.value} onChange={email.onChange} />
       </div>
       <div className="row">
         <div>
-          <input
-            placeholder="EID"
-            value={eid}
-            onChange={handleEIDChange}
-          />
+          <input placeholder="EID" value={eid.value} onChange={eid.onChange} />
           <br />
-          {!eidValid &&
-            <p class="form-text alert-danger">
-              Hey stop pressing 9!
-                </p>
-          }
+          {!eid.valid && (
+            <p class="form-text alert-danger">Hey stop pressing 9!</p>
+          )}
         </div>
       </div>
       <div className="row">
         <h2>
-          {props.first} {props.last} ({email}) #{eid}
+          {props.first} {props.last} ({email.value}) #{eid.value}
         </h2>
       </div>
     </div>
   );
+}
+
+// hook for EID plus validation
+function useEid() {
+  const [value, setValue] = useState('');
+  const [valid, setValid] = useState(true);
+
+  function onChange(e) {
+    const { value } = e.target;
+
+    if (value.includes('9')) {
+      setValid(false);
+    } else {
+      setValid(true);
+      setValue(value);
+    }
+  }
+
+  return {
+    value,
+    valid,
+    onChange
+  };
+}
+
+// no validation version of above.  Very reusable.
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    onChange: (e) => setValue(e.target.value)
+  };
 }
