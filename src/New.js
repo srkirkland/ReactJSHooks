9c9d432e2f20e4886a5e7a064d66bf6502564499
Unknown <srkirkland@ucdavis.edu>
2019-04-04 11:21:40 -0700

    eid text field with validation

--- src/New.js
+++ src/New.js
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 
 export default function(props) {
   const [email, setEmail] = useState(props.first + '@gmail.com');
+  const [eid, setEid] = useState('');
+  const [eidValid, setEidValid] = useState(true);
 
   const handleEmailChange = e => setEmail(e.target.value);
+  const handleEIDChange = e => {
+    const eid = e.target.value;
+
+    if (eid.includes('9')) {
+      setEidValid(false);
+    } else {
+      setEidValid(true)
+      setEid(eid);
+    }
+  }
 
   return (
     <div>
@@ -11,8 +23,23 @@ export default function(props) {
         <input value={email} onChange={handleEmailChange} />
       </div>
       <div className="row">
+        <div>
+          <input
+            placeholder="EID"
+            value={eid}
+            onChange={handleEIDChange}
+          />
+          <br />
+          {!eidValid &&
+            <p class="form-text alert-danger">
+              Hey stop pressing 9!
+                </p>
+          }
+        </div>
+      </div>
+      <div className="row">
         <h2>
-          {props.first} {props.last} ({email})
+          {props.first} {props.last} ({email}) #{eid}
         </h2>
       </div>
     </div>
--- src/Old.js
+++ src/Old.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 
-// 2. Class with state
+// 3. Class with state and validation
 export default class Old extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: props.first + '@gmail.com'
+      email: props.first + '@gmail.com',
+      eid: '',
+      eidValid: true
     };
   }
 
   handleEmailChange = e => {
     this.setState({ email: e.target.value });
+  };
+
+  handleEIDChange = e => {
+    // custom logic to validate e.  no 9's!
+    const eid = e.target.value;
+
+    if (eid.includes('9')) {
+      this.setState({ eidValid: false });
+    } else {
+      this.setState({ eidValid: true, eid });
+    }
   };
 
   render() {
@@ -21,8 +34,24 @@ export default class Old extends Component {
           <input value={this.state.email} onChange={this.handleEmailChange} />
         </div>
         <div className="row">
+          <div>
+            <input
+              placeholder="EID"
+              value={this.state.eid}
+              onChange={this.handleEIDChange}
+            />
+            <br />
+            { !this.state.eidValid && 
+                <p class="form-text alert-danger">
+                Hey stop pressing 9!
+                </p>
+            }
+          </div>
+        </div>
+        <div className="row">
           <h2>
-            {this.props.first} {this.props.last} ({this.state.email})
+            {this.props.first} {this.props.last} ({this.state.email}) #
+            {this.state.eid}
           </h2>
         </div>
       </div>
